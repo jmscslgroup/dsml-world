303fe5aa23ab81992e66dcefb05c8b5da622fcc0
jillalexander <jillalexander@email.arizona.edu>
2019-08-06 06:30:57 -0700

    updated toObject.py

--- toObject.py
+++ toObject.py
@@ -6,6 +6,7 @@ from geometry_msgs.msg import Twist, Pose
 import sys, getopt, math
 
 PI = 3.1415926535897
+#Controller to move towards or away from an object - similar to avoidObject.py, but it meant for Gazebo catvehicle model
 
 class catvehicle:
     
@@ -14,7 +15,7 @@ class catvehicle:
         rospy.init_node('catvehicle_controller', anonymous=True)
         
         #publisher to cmd_vel topic
-        self.velocity_publisher = rospy.Publisher('/catvehicle/cmd_vel', Twist, queue_size = 10)
+        self.velocity_publisher = rospy.Publisher('/catvehicle/cmd_vel', Twist, queue_size = 1)
         
         #subsciber to dist, called when a Float64 message is recieved
         self.dist_subscriber = rospy.Subscriber('/catvehicle/distanceEstimatorSteeringBased/dist', Float64, self.update_dist)
@@ -27,6 +28,7 @@ class catvehicle:
         self.angle = Float64()
         self.rate = rospy.Rate(10)
         
+        
     def update_dist(self, data):
         self.dist = data
         
@@ -38,48 +40,62 @@ class catvehicle:
         print("Press 1 to begin. ")
         isReady = input("Begin?: ")
         
-        speed = 3.0
+        speed = 5.0
         minDist = 5.0
         
         currentDist = Float64()
         vel_msg = Twist()
         
+        #initialize everything but angular.z and linear.x to 0
         vel_msg.linear.y = 0
         vel_msg.linear.z = 0
         
         vel_msg.angular.x = 0
         vel_msg.angular.y = 0
-        vel_msg.angular.z = self.angle.data
-        #set angular velocity to towwrd nearest object - if you want it to go away, set as negative (possibly)
-        #radians or degrees?
-        print(self.angle.data)
         
-        print('Test 0 before if statement')
+        #converting input angle from rads to degress
+        degAngle = self.angle.data*(180/PI)
+        
+        #print distance and angle data
+        print(self.angle.data)
+        print(degAngle)
         
         if(isReady):
             vel_msg.linear.x = speed
+            vel_msg.angular.z = speed
         else:
             vel_msg.linear.x = 0
+            vel_msg.angular.z = 0
             
         while not rospy.is_shutdown():
             t0 = rospy.Time.now().to_sec()
-            print("Test 1 first while loop")
             currentDist = self.dist.data
             print(currentDist)
                 
             while(currentDist >= minDist):
-                #publish the velocity
-                self.velocity_publisher.publish(vel_msg)
+                print(self.dist.data)
+                #if catvehicle steers away, steer back towards the closest object
+                if(degAngle < 1.0 and degAngle > -1.0):
+                    vel_msg.angular.z = 0
+                elif(degAngle < -1.0):
+                    vel_msg.angular.z = speed
+                else:
+                    vel_msg.angular.z = -speed
+                print(self.angle.data)
                 #update current distance
                 currentDist = self.dist.data
-                print(self.dist.data)
+                #update angle
+                degAngle = self.angle.data
+                #publish the velocity
+                self.velocity_publisher.publish(vel_msg)
+                
                 #update publishing rate
                 self.rate.sleep()
             
-            print("Test 3 exited 2nd while loop")
             vel_msg.linear.x = 0
             self.velocity_publisher.publish(vel_msg)
-            print("Test 4 published velocity")
+            if(currentDist <= minDist):
+                print("Script exited: TOO CLOSE")
             
             rospy.spin()
  
@@ -90,5 +106,3 @@ if __name__ == '__main__':
         x.move2object()
     except rospy.ROSInterruptException:
         pass
-        
-        
